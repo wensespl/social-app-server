4d23ce7357207bdbcbafc082781d7b8671eaf4a8
wensespl <wenses400@gmail.com>
2022-08-22 21:53:38 -0500

    add auth routes

--- src/routes/AuthRouters.js
+++ src/routes/AuthRouters.js
@@ -1,5 +1,14 @@
 const router = require('express').Router()
 
-router.route('/').get()
+const {
+  register,
+  login,
+  renewToken
+} = require('../controllers/AuthControllers')
+const { validateJWT } = require('../middlewares/JWT')
+
+router.route('/register').post(register)
+router.route('/login').post(login)
+router.get('/renew', validateJWT, renewToken)
 
 module.exports = router
