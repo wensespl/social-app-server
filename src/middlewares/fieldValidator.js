e4c154be386e60163783568ca36dbb6bfcc48183
wensespl <wenses400@gmail.com>
2022-08-22 21:58:20 -0500

    add field validator

---
+++ src/middlewares/fieldValidator.js
@@ -0,0 +1,20 @@
+const { validationResult } = require('express-validator')
+
+const errorFormatter = ({ location, msg, param }) => {
+  return `${location}[${param}]: ${msg}`
+}
+
+const fieldValidator = (req, res, next) => {
+  const errors = validationResult(req).formatWith(errorFormatter)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    })
+  }
+  next()
+}
+
+module.exports = {
+  fieldValidator
+}
